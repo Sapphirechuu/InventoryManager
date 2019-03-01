581f1f67bb9385102b5ea09c6fdc60b4a962e2c3
Sapphire Fowler <kylespell101@gmail.com>
2019-03-01 15:19:40 -0800

    Finished Assessment

--- InventoryManager/MainWindow.xaml.cs
+++ InventoryManager/MainWindow.xaml.cs
@@ -13,13 +13,32 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using System.IO;
+using Microsoft.Win32;
 
 namespace InventoryManager
 {
+    class loadFile
+    {
+        private string boxName;
+        private string item;
+
+        public string BoxName{ get => boxName; set => boxName = value; }
+        public string Item { get => item; set => item = value; }
+
+        public loadFile(string v1, string v2)
+        {
+            BoxName = v1;
+            Item = v2;
+        }
+    }
+
     public partial class MainWindow : Window
     {
 
         public ObservableCollection<string> options = new ObservableCollection<string>();
+        List<loadFile> loadFiles = new List<loadFile>();
 
         public int rowNum;
         public int columnNum;
@@ -30,9 +49,21 @@ namespace InventoryManager
             options.Add("Deadpool");
             options.Add("Hello Kitty");
             options.Add("Storm");
+            options.Add("X-23");
+            options.Add("Wolverine");
+            options.Add("Stan Lee");
+            options.Add("Raven");
+            options.Add("Batman");
+            options.Add("Wonder Woman");
+            options.Add("Uno");
 
-            _0x0Box.ItemsSource = options;
-            _0x1Box.ItemsSource = options;
+            string temp = "";
+            for(int i = 0; i < 24; i++)
+            {
+                temp = "_0x" + i.ToString() + "Box";
+                object tempOb = FindName(temp);
+                ((ComboBox)tempOb).ItemsSource = options;
+            }
         }
 
         //private void RowBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -54,11 +85,79 @@ namespace InventoryManager
             BitmapImage bitmapImage = new BitmapImage();
 
             bitmapImage.BeginInit();
-            bitmapImage.UriSource = new Uri(@"C:\Users\s189075\Downloads\" + ((ComboBox)sender).SelectedItem + ".jpg");
+            if (File.Exists(System.IO.Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName) + "/Pictures/" + ((ComboBox)sender).SelectedItem + ".jpg"))
+            {
+                bitmapImage.UriSource = new Uri(System.IO.Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName) + "/Pictures/" + ((ComboBox)sender).SelectedItem + ".jpg");
 
-            bitmapImage.EndInit();
-            ((Image)wanted).Source = bitmapImage;
+                bitmapImage.EndInit();
+                ((Image)wanted).Source = bitmapImage;
+            }
+            else
+            {
+                ((ComboBox)sender).SelectedItem = null;
+                ((Image)wanted).Source = null;
+            }
+        }
 
+        private void TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Regex.IsMatch(((TextBox)sender).Text, @"^[0-9]+$"))
+            {
+                if ((Convert.ToInt32(((TextBox)sender).Text)) >= 100)
+                {
+                    ((TextBox)sender).Text = "99";
+                }
+            }
+        }
+         
+        private void OnSaveClick(object sender, RoutedEventArgs e)
+        {
+            IEnumerable<ComboBox> comboBoxes = MainGrid.Children.OfType<ComboBox>();
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text files (*.txt;)|*.txt;|All files (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                using (StreamWriter sw = File.CreateText(saveFileDialog.FileName))
+                {
+                    foreach (ComboBox combo in comboBoxes)
+                    {
+                        if (combo.SelectedItem != null)
+                        {
+                            sw.WriteLine(combo.Name + "," + combo.SelectedItem.ToString());
+                        }
+                        else
+                        {
+                            sw.WriteLine(combo.Name + ",null");
+                        }
+                    }
+                }
+            }
+        }
+
+        private void OnLoadClick(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Click");
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string[] temp;
+                string[] lines = File.ReadAllLines(openFileDialog.FileName);
+                foreach(string s in lines)
+                {
+                    temp = s.Split(',');
+                    string temps;
+                    temps = (temp[0]);
+                    object tempOb = FindName(temps);
+                    if (temp[1] != "null")
+                    {
+                        ((ComboBox)tempOb).SelectedItem = temp[1];
+                    }
+                    else
+                    {
+                        ((ComboBox)tempOb).SelectedItem = null;
+                    }
+                }
+            }
         }
     }
 }
